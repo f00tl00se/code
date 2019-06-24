780fe3e98b2a422ec7f3b46fb8b43fe2e2fdf9c6
rong.li <footloose@qq.com>
2019-06-24 18:26:58 +0800

    chapter2 增加中文注释

--- chapter2/sample/search/search.go
+++ chapter2/sample/search/search.go
@@ -6,35 +6,43 @@ import (
 )
 
 // A map of registered matchers for searching.
+// 注册用于搜索的匹配器的映射
 var matchers = make(map[string]Matcher)
 
 // Run performs the search logic.
 func Run(searchTerm string) {
 	// Retrieve the list of feeds to search through.
+	// 获取需要搜索的数据源列表
 	feeds, err := RetrieveFeeds()
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// Create an unbuffered channel to receive match results to display.
+	// 创建一个无缓冲的通道，接收匹配后的结果去显示
 	results := make(chan *Result)
 
 	// Setup a wait group so we can process all the feeds.
+	// 构造一个wait group，以便处理所有的数据源
 	var waitGroup sync.WaitGroup
 
 	// Set the number of goroutines we need to wait for while
 	// they process the individual feeds.
+	// 设置需要等待处理每个数据源的goroutine的数量
 	waitGroup.Add(len(feeds))
 
 	// Launch a goroutine for each feed to find the results.
+	// 为每个数据源启动一个goroutine来查找结果
 	for _, feed := range feeds {
 		// Retrieve a matcher for the search.
+		// 获取一个匹配器用于查找
 		matcher, exists := matchers[feed.Type]
 		if !exists {
 			matcher = matchers["default"]
 		}
 
 		// Launch the goroutine to perform the search.
+		// 启动一个 goroutine来执行搜索
 		go func(matcher Matcher, feed *Feed) {
 			Match(matcher, feed, searchTerm, results)
 			waitGroup.Done()
@@ -42,21 +50,26 @@ func Run(searchTerm string) {
 	}
 
 	// Launch a goroutine to monitor when all the work is done.
+	// 启动一个goroutine来监控是否所有的工作都做完了
 	go func() {
 		// Wait for everything to be processed.
+		// 等候所有任务完成
 		waitGroup.Wait()
 
 		// Close the channel to signal to the Display
 		// function that we can exit the program.
+		// 用关闭通道的方式，通知Display函数可以退出程序了
 		close(results)
 	}()
 
 	// Start displaying results as they are available and
 	// return after the final result is displayed.
+	// 启动函数，显示返回的结果，并且在最后一个结果显示完后返回
 	Display(results)
 }
 
 // Register is called to register a matcher for use by the program.
+// Register调用时，会注册一个匹配器，提供给后面的程序使用
 func Register(feedType string, matcher Matcher) {
 	if _, exists := matchers[feedType]; exists {
 		log.Fatalln(feedType, "Matcher already registered")
